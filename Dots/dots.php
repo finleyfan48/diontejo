cefdd4731fa5e6c1fd14c4ee24c801512539df9f
finleyfan48 <dionte.johnson0407@gmail.com>
2017-02-10 17:26:22 -0600

    added initial files

---
+++ Dots/dots.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=1200">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<title>Dots</title>
+	<meta name="author" content="Dionte Johnson">
+
+	<link href="less/styles.css" rel="stylesheet" type="text/css" />
+</head>
+	<body>
+		<div id='debug'>[debug]</div>
+		<script type="text/javascript" src="scripts/code.js"></script>
+	</body>
+</html>
+
+
